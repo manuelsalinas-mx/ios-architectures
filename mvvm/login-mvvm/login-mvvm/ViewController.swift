59326b7bbb7cec24a12e3d5f01063fc5bdfd67e8
Manu Salinas <manuelsalinas.mx@gmail.com>
2024-01-09 09:56:40 -0600

    init login MVVM

---
+++ mvvm/login-mvvm/login-mvvm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  login-mvvm
+//
+//  Created by Manuel Salinas on 1/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
